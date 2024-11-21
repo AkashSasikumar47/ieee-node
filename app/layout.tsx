d07a96d586262edc3e55546738a730693432ee2c
AkashSasikumar47 <akashsasikumar47@gmail.com>
2024-11-21 16:46:59 +0400

    Initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from "next";
+import { Montserrat } from "next/font/google";
+import "./globals.css";
+
+const montserrat = Montserrat({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "AR-ONE",
+  description: "Explore the future of artificial intelligence with hands-on experience in Generative AI.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+      </head>
+      <body className={montserrat.className}>{children}</body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <h1 className="text-3xl text-green-500 font-bold underline">
+      Hello world!
+    </h1>
+  )
+}
